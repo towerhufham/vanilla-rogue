c5fe5e1cc2ca8ec7b632f1a2e9caec1ea1707d22
towerhufham <32599109+towerhufham@users.noreply.github.com>
2017-11-03 14:12:27 -0400

    Implementing tile and entity classes

--- vanilla-rogue.js
+++ vanilla-rogue.js
@@ -1,181 +1,100 @@
+function createArray(length) { //this function by Matthew Crumley
+    var arr = new Array(length || 0),
+        i = length;
+
+    if (arguments.length > 1) {
+        var args = Array.prototype.slice.call(arguments, 1);
+        while(i--) arr[length-1 - i] = createArray.apply(this, args);
+    }
+
+    return arr;
+}
+
 var Game = {
     display: null,
-    map: {},
+	width: 50,
+	height: 30,
+    map: null,
     engine: null,
     player: null,
-    pedro: null,
-    ananas: null,
     
     init: function() {
-        this.display = new ROT.Display({spacing:1.1});
+	this.display = new ROT.Display({width:this.width, height:this.height, fontSize:32});
         document.body.appendChild(this.display.getContainer());
         
-        this._generateMap();
+		this.map = createArray(this.width, this.height);
+        this.generateMap();
         
         var scheduler = new ROT.Scheduler.Simple();
-        scheduler.add(this.player, true);
-        scheduler.add(this.pedro, true);
 
         this.engine = new ROT.Engine(scheduler);
         this.engine.start();
+		
+		this.drawMap();
     },
     
-    _generateMap: function() {
-        var digger = new ROT.Map.Digger();
+    generateMap: function() {
+        var generator = new ROT.Map.Rogue(this.width, this.height);
         var freeCells = [];
-        
-        var digCallback = function(x, y, value) {
+        var floor = new Tile(".", "#777", "#000")
+		
+        var callback = function(x, y, value) {
             if (value) { return; }
             
-            var key = x+","+y;
-            this.map[key] = ".";
-            freeCells.push(key);
+            this.map[x][y] = floor;
+            freeCells.push({x:x, y:y});
         }
-        digger.create(digCallback.bind(this));
-        
-        this._generateBoxes(freeCells);
-        this._drawWholeMap();
-        
-        this.player = this._createBeing(Player, freeCells);
-        this.pedro = this._createBeing(Pedro, freeCells);
+        generator.create(callback.bind(this));
     },
-    
-    _createBeing: function(what, freeCells) {
-        var index = Math.floor(ROT.RNG.getUniform() * freeCells.length);
-        var key = freeCells.splice(index, 1)[0];
-        var parts = key.split(",");
-        var x = parseInt(parts[0]);
-        var y = parseInt(parts[1]);
-        return new what(x, y);
-    },
-    
-    _generateBoxes: function(freeCells) {
-        for (var i=0;i<10;i++) {
-            var index = Math.floor(ROT.RNG.getUniform() * freeCells.length);
-            var key = freeCells.splice(index, 1)[0];
-            this.map[key] = "*";
-            if (!i) { this.ananas = key; } /* first box contains an ananas */
-        }
-    },
-    
-    _drawWholeMap: function() {
-        for (var key in this.map) {
-            var parts = key.split(",");
-            var x = parseInt(parts[0]);
-            var y = parseInt(parts[1]);
-            this.display.draw(x, y, this.map[key]);
-        }
-    }
-};
-
-var Player = function(x, y) {
-    this._x = x;
-    this._y = y;
-    this._draw();
+	
+	drawMap: function() {
+		for (var y = 0; y < this.height; y++) {
+			for (var x = 0; x < this.width; x++) {
+				//alert(this.map[x][y]);
+				if (this.map[x][y]) {
+					var t = this.map[x][y];
+					this.display.draw(x, y, t.character, t.fg, t.bg);
+				} else {
+					this.display.draw(x, y, " ", "#fff", "#000");
+				}
+			}
+		}
+	}
 }
     
-Player.prototype.getSpeed = function() { return 100; }
-Player.prototype.getX = function() { return this._x; }
-Player.prototype.getY = function() { return this._y; }
-
-Player.prototype.act = function() {
-    Game.engine.lock();
-    window.addEventListener("keydown", this);
-}
-    
-Player.prototype.handleEvent = function(e) {
-    var code = e.keyCode;
-    if (code == 13 || code == 32) {
-        this._checkBox();
-        return;
-    }
-
-    var keyMap = {};
-    keyMap[38] = 0;
-    keyMap[33] = 1;
-    keyMap[39] = 2;
-    keyMap[34] = 3;
-    keyMap[40] = 4;
-    keyMap[35] = 5;
-    keyMap[37] = 6;
-    keyMap[36] = 7;
-
-    /* one of numpad directions? */
-    if (!(code in keyMap)) { return; }
-
-    /* is there a free space? */
-    var dir = ROT.DIRS[8][keyMap[code]];
-    var newX = this._x + dir[0];
-    var newY = this._y + dir[1];
-    var newKey = newX + "," + newY;
-    if (!(newKey in Game.map)) { return; }
-
-    Game.display.draw(this._x, this._y, Game.map[this._x+","+this._y]);
-    this._x = newX;
-    this._y = newY;
-    this._draw();
-    window.removeEventListener("keydown", this);
-    Game.engine.unlock();
+class Tile { 
+	constructor(character, fg, bg, passable=true) {
+		this.character = character;
+		this.fg = fg;
+		this.bg = bg;
+		this.passable;
+	}
 }
 
-Player.prototype._draw = function() {
-    Game.display.draw(this._x, this._y, "@", "#ff0");
+class Entity {
+	constructor(config) {
+		if ("name" in config) {
+			this.name = config.name;
+		} else {
+			this.name = "No name";
+		}
+		
+		if ("character" in config) {
+			this.character = config.character;
+		} else {
+			this.character = "?";
+		}
+		
+		if ("fg" in config) {
+			this.fg = config.fg;
+		} else {
+			this.fg = "#fff";
+		}
+		
+		if ("bg" in config) {
+			this.bg = config.bg;
+		} else {
+			this.bg = "#000";
+		}
+	}
 }
-    
-Player.prototype._checkBox = function() {
-    var key = this._x + "," + this._y;
-    if (Game.map[key] != "*") {
-        alert("There is no box here!");
-    } else if (key == Game.ananas) {
-        alert("Hooray! You found an ananas and won this game.");
-        Game.engine.lock();
-        window.removeEventListener("keydown", this);
-    } else {
-        alert("This box is empty :-(");
-    }
-}
-    
-var Pedro = function(x, y) {
-    this._x = x;
-    this._y = y;
-    this._draw();
-}
-    
-Pedro.prototype.getSpeed = function() { return 100; }
-    
-Pedro.prototype.act = function() {
-    var x = Game.player.getX();
-    var y = Game.player.getY();
-
-    var passableCallback = function(x, y) {
-        return (x+","+y in Game.map);
-    }
-    var astar = new ROT.Path.AStar(x, y, passableCallback, {topology:4});
-
-    var path = [];
-    var pathCallback = function(x, y) {
-        path.push([x, y]);
-    }
-    astar.compute(this._x, this._y, pathCallback);
-
-    path.shift();
-    if (path.length == 1) {
-        Game.engine.lock();
-        alert("Game over - you were captured by Pedro!");
-    } else {
-        x = path[0][0];
-        y = path[0][1];
-        Game.display.draw(this._x, this._y, Game.map[this._x+","+this._y]);
-        this._x = x;
-        this._y = y;
-        this._draw();
-    }
-}
-    
-Pedro.prototype._draw = function() {
-    Game.display.draw(this._x, this._y, "P", "red");
-}    
-
-
-Game.init();
-
